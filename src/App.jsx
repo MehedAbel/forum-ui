4e765a920dd42bed4a080d56f7cd43bb14597f97
Abel <111386807+abel325@users.noreply.github.com>
2024-10-07 19:03:31 +0300

    test api connection

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home/Home.jsx'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +33,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { API_URL } from "../../config.js";
+
+const Home = () => {
+
+    const url = `${API_URL}`;
+    const [users, setUsers] = useState([]);
+
+    const loadUsersList = async () => {
+        fetch(`${API_URL}/users`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data && data.length > 0) {
+                    const tempData = [];
+                    data.forEach((item) => {
+                        tempData.push({
+                            id: item.id,
+                            username: item.username,
+                            email: item.email
+                        });
+                    });
+                    setUsers(tempData);
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    };
+
+    useEffect(() => {
+        loadUsersList();
+    }, []);
+
+    return (
+        <div>
+            <h1>Home, the URL is: {url}</h1>
+            <h1>Home, the URL is THERE?</h1>
+            <ul>
+                {users.map((user, index) => (
+                    <li key={index}>
+                        {user.id} - {user.username} - {user.email}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Home;
